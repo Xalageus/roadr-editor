188cf957b8949162342323e2b6b7c5bb7f33f2d0
Xalageus <xalageus@gmail.com>
2020-01-08 18:26:02 -0600

    remove sdl variable

--- roadr_editor/__main__.py
+++ roadr_editor/__main__.py
@@ -1,10 +1,8 @@
 import sys
-import os
 #from PySide2.QtWidgets import *
 from PyQt5.QtWidgets import *
 from roadr_editor.mainController import mainController
 
-os.environ["SDL_VIDEODRIVER"] = "windib"
 app = QApplication([])
 form = mainController()
 form.show()
