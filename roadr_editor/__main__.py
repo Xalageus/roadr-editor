f91cd1a10a81727891e11f66ff4f5597f0344dfb
Xalageus <xalageus@gmail.com>
2020-01-09 20:16:12 -0600

    Use main function

--- roadr_editor/__main__.py
+++ roadr_editor/__main__.py
@@ -1,9 +1,12 @@
 import sys
-#from PySide2.QtWidgets import *
 from PyQt5.QtWidgets import *
 from roadr_editor.mainController import mainController
 
-app = QApplication([])
-form = mainController()
-form.show()
-sys.exit(app.exec_())
+def main():
+    app = QApplication([])
+    form = mainController()
+    form.show()
+    sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
