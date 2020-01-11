2f908a83da756078fa10d9251c16e9d2b68789da
Xalageus <xalageus@gmail.com>
2020-01-11 01:02:36 -0600

    buildui clean

--- buildui.bat
+++ buildui.bat
@@ -1,3 +1,9 @@
 @echo off
-pyuic5 roadr_editor\main.ui -o roadr_editor\main.py
-pyuic5 roadr_editor\settings.ui -o roadr_editor\settings.py
+
+IF "%1" == "clean" (
+    del roadr_editor\main.py
+    del roadr_editor\settings.py
+) ELSE (
+    pyuic5 roadr_editor\main.ui -o roadr_editor\main.py
+    pyuic5 roadr_editor\settings.ui -o roadr_editor\settings.py
+)
