099f0e513801eb97351283ee84d4a2fa3c9bafda
Xalageus <xalageus@gmail.com>
2020-01-11 00:12:29 -0600

    Call parent ui on mouse move

--- roadr_editor/graphicsTileView.py
+++ roadr_editor/graphicsTileView.py
@@ -4,6 +4,11 @@ from PyQt5 import QtGui, QtCore
 class GraphicsTileView(QGraphicsView):
     def __init__(self, parent):
         super(GraphicsTileView, self).__init__(parent)
+        self.uiParent = None
+
+    def setUiParent(self, parent):
+        self.uiParent = parent
 
     def mouseMoveEvent(self, event):
+        self.uiParent.graphicsMouseMoveEvent()
         print(event.pos())
